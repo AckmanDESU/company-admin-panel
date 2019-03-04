382678aea00fb12ea994fae583d15ee58455c877
Mario <torinoob@gmail.com>
2019-03-04 16:43:46 +0100

    Configuración de Docker, Homestead y BrowserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,20 +16,21 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js');
 
-mix.sass('resources/sass/app.scss', 'public/css')
+mix.sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({
-    host: '192.168.10.10',
-    proxy: 'homestead.test',
-    open: false,
-    files: [
-      'app/**/*.php',
-      'resources/views/**/*.php',
-      'public/js/**/*.js',
-      'public/css/**/*.css'
-    ],
-    watchOptions: {
-        usePolling: true,
-        interval: 500
-    }
+  proxy: 'app',
+  host: process.env.MIX_DOCKER_IP || '127.0.0.1',
+  open: false,
+  files: [
+    'app/**/*.php',
+    'resources/views/**/*.php',
+    'public/js/**/*.js',
+    'public/css/**/*.css',
+    'node_modules/**/*.js',
+  ],
+  watchOptions: {
+    usePolling: true,
+    interval: 500,
+  },
 });
